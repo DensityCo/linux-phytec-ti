d849c35fd11eb6bed9195d653a8941f6701995c3
Russell Robinson <rrobinson@phytec.com>
2015-04-21 13:04:42 -0700

    am57xx: clk-7xx: configure the dcan1 clk mux
    Signed-off-by: Russell Robinson <rrobinson@phytec.com>

--- drivers/clk/ti/clk-7xx.c
+++ drivers/clk/ti/clk-7xx.c
@@ -317,10 +317,26 @@ int __init dra7xx_dt_clk_init(void)
 	int rc;
 	struct clk *dpll_ck, *hdcp_ck;
 	struct clk *atl_fck, *atl_parent;
+	struct clk *dcan1_fck, *sys_clkin1;
 
 	ti_dt_clocks_register(dra7xx_clks);
 
 	omap2_clk_disable_autoidle_all();
+	
+	abe_dpll_mux = clk_get_sys(NULL, "abe_dpll_sys_clk_mux");
+	sys_clkin2 = clk_get_sys(NULL, "sys_clkin2");
+	dpll_ck = clk_get_sys(NULL, "dpll_abe_ck");
+
+	rc = clk_set_parent(abe_dpll_mux, sys_clkin2);
+	if (!rc)
+		rc = clk_set_rate(dpll_ck, DRA7_DPLL_ABE_DEFFREQ);
+	if (rc)
+		pr_err("%s: failed to configure ABE DPLL!\n", __func__);
+
+	dpll_ck = clk_get_sys(NULL, "dpll_abe_m2x2_ck");
+	rc = clk_set_rate(dpll_ck, DRA7_DPLL_ABE_DEFFREQ * 2);
+	if (rc)
+		pr_err("%s: failed to configure ABE DPLL m2x2!\n", __func__);
 
 	dpll_ck = clk_get_sys(NULL, "dpll_gmac_ck");
 	rc = clk_set_rate(dpll_ck, DRA7_DPLL_GMAC_DEFFREQ);
@@ -336,6 +352,12 @@ int __init dra7xx_dt_clk_init(void)
 	rc = clk_set_rate(dpll_ck, DRA7_DPLL_USB_DEFFREQ/2);
 	if (rc)
 		pr_err("%s: failed to set USB_DPLL M2 OUT\n", __func__);
+
+	dcan1_fck = clk_get_sys(NULL, "dcan1_sys_clk_mux");
+	sys_clkin1 = clk_get_sys(NULL, "sys_clkin1");
+	rc = clk_set_parent(dcan1_fck, sys_clkin1);
+	if (rc)
+		pr_err("%s: failed to set dcan1_sys_clk_mux\n", __func__);
 
 	hdcp_ck = clk_get_sys(NULL, "dss_deshdcp_clk");
 	rc = clk_prepare_enable(hdcp_ck);
