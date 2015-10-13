9fbc208af69d9cd040f8ab8f681aed1e344b1f1b
Russell Robinson <rrobinson@phytec.com>
2015-10-13 12:24:56 -0700

    pci-dra7xx: add dts option for enabling clocks
    Added support to control the output enable for
    the PCIe clock generator IC on the PHYTEC
    phyCORE-AM57xx RDK. The output enable pin is
    connected to a GPIO.
    
    clk_oe is obtained from the dts via the second gpios entry,
    and it is optional so this will not break functionality
    for any other boards.
    
    Signed-off-by: Russell Robinson <rrobinson@phytec.com>

--- drivers/pci/host/pci-dra7xx.c
+++ drivers/pci/host/pci-dra7xx.c
@@ -340,11 +340,11 @@ static int __init dra7xx_pcie_probe(struct platform_device *pdev)
 	struct phy **phy;
 	void __iomem *base;
 	struct resource *res;
+	struct gpio_desc *reset, *clk_oe;
 	struct dra7xx_pcie *dra7xx;
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
 	char name[10];
-	struct gpio_desc *reset;
 
 	dra7xx = devm_kzalloc(dev, sizeof(*dra7xx), GFP_KERNEL);
 	if (!dra7xx)
@@ -407,10 +407,21 @@ static int __init dra7xx_pcie_probe(struct platform_device *pdev)
 		goto err_get_sync;
 	}
 
-	reset = devm_gpiod_get_optional(dev, NULL, GPIOD_OUT_HIGH);
+	clk_oe = devm_gpiod_get_optional(dev, "pcie-clk-oe", GPIOD_OUT_HIGH);
+	if (IS_ERR(clk_oe)) {
+		ret = PTR_ERR(clk_oe);
+		dev_err(&pdev->dev, "clk_oe gpio request failed, ret %d\n", ret);
+		goto err_gpio;
+	}
+
+	if (of_property_read_bool(np, "pcie-reset-active-low"))
+		reset = devm_gpiod_get_optional(dev, "pcie-reset", GPIOD_OUT_LOW);
+	else
+		reset = devm_gpiod_get_optional(dev, "pcie-reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(reset)) {
 		ret = PTR_ERR(reset);
 		dev_err(&pdev->dev, "gpio request failed, ret %d\n", ret);
+
 		goto err_gpio;
 	}
 
