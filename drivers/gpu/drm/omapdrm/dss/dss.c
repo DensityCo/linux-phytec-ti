9b895042c368cdbf55e7fd8fbcf14e8827d3cc08
Russell Robinson <rrobinson@phytec.com>
2016-10-13 09:40:35 -0700

    AM57X-131 gpu: drm: omapdrm: dss: support other VOUT ports
    Allow dss_dpi_select_source_dra7xx to set the channel
    in the AM57xx DSS_CONTROL register for any VOUT port,
    not just VOUT1 (port 0).
    
    Signed-off-by: Russell Robinson <rrobinson@phytec.com>

--- drivers/gpu/drm/omapdrm/dss/dss.c
+++ drivers/gpu/drm/omapdrm/dss/dss.c
@@ -804,7 +804,9 @@ static int dss_dpi_select_source_dra7xx(int port, enum omap_channel channel)
 {
 	switch (port) {
 	case 0:
-		return dss_dpi_select_source_omap5(port, channel);
+		if (channel != OMAP_DSS_CHANNEL_LCD)
+			return -EINVAL;
+		break;
 	case 1:
 		if (channel != OMAP_DSS_CHANNEL_LCD2)
 			return -EINVAL;
@@ -817,7 +819,7 @@ static int dss_dpi_select_source_dra7xx(int port, enum omap_channel channel)
 		return -EINVAL;
 	}
 
-	return 0;
+	return dss_dpi_select_source_omap5(port, channel);
 }
 
 int dss_dpi_select_source(int port, enum omap_channel channel)
